f6090b5752cafc064bcb92b7bb91b74648102444
DuStein <steindu@gmail.com>
2022-12-13 16:05:26 -0300

    debigando

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
+  console.log('Acesso na raiz')
   res.json({rodando: "Rota RAIZ. AWS Server"})
 });
 
 app.get('/rota', (req, res) => {
+  console.log('acesso na /rota')
   res.send('está na ROTA')
 })
 
