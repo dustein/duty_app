ae994bf3da9fbe55e6c4d207769793e446772b6b
DuStein <steindu@gmail.com>
2022-12-13 15:24:41 -0300

    testando

--- src/server.js
+++ src/server.js
@@ -7,5 +7,9 @@ app.get('/', (req, res) => {
   res.json({rodando: "Rota RAIZ. AWS Server"})
 });
 
+app.get('/rota', (req, res) => {
+  res.send('está na ROTA')
+})
+
 app.listen(3030, () => { console.log('Servidor AWS Rodando porta 3030') });
 
